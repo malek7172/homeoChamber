fba1da2828c44f546da98004d35ed66c6995ff3d
malek7172 <amalek43@gmail.com>
2026-02-03 06:59:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("chamber").then(c =>
-      c.addAll([
-        "/",
-        "/index.html",
-        "/dashboard.html",
-        "/prescription.html",
-        "/payment.html"
-      ])
-    )
-  );
-});
+
